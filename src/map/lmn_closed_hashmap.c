3232ebba7ba8d98c946d65c8e8161bbda487a11b
Taketo Yoshida <tamanyan.sss@gmail.com>
2013-10-10 12:57:38 +0900

    adjust olivet

--- src/map/lmn_closed_hashmap.c
+++ src/map/lmn_closed_hashmap.c
@@ -8,42 +8,6 @@
 
 #define LMN_CLOSED_INIT_SIZE (32 * 1024 * 1024)
 
-#ifdef __linux__
-
-inline lmn_hash_t lmn_hash_calc(lmn_key_t key) {
-#ifdef __x86_64__
-  /* Robert Jenkins' 32 bit Mix Function */
-  key += (key << 12);
-  key ^= (key >> 22);
-  key += (key << 4);
-  key ^= (key >> 9);
-  key += (key << 10);
-  key ^= (key >> 2);
-  key += (key << 7);
-  key ^= (key >> 12);
-
-  /* Knuth's Multiplicative Method */
-  key = (key >> 3) * 2654435761;
-#else
-  key ^= (key << 15) ^ 0xcd7dcd7d;
-  key ^= (key >> 10);
-  key ^= (key <<  3);
-  key ^= (key >>  6);
-  key ^= (key <<  2) + (key << 14);
-  key ^= (key >> 16);
-#endif
-  return key;
-}
-
-inline int lmn_hash_eq(lmn_hash_t h1, lmn_hash_t h2) {
-  return h1 == h2;
-}
-
-inline int lmn_hash_key_eq(lmn_hash_t key1, lmn_hash_t key2) {
-  return key1 == key2;
-}
-
-#endif
 /*
  * private functions
  */
